e25466e9b213ae019b943c47aff9fa37cbc51d0c
MattLV21 <Mattias.lange00@gmail.com>
2023-02-13 15:36:56 +0100

    Update Puslespil.java

--- DM507/Exercises/Puslespil.java
+++ DM507/Exercises/Puslespil.java
@@ -7,30 +7,63 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Puslespil {
+    Integer[] intArray;
     public static void main(String[] args) {
-        Integer[] intArray;
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Give me an Integer! ");
         int n = input.nextInt();
         input.close();
 
-        intArray = new Integer[n];
+        Puslespil spil = new Puslespil(n);
+		System.out.println(Arrays.toString(spil.intArray));
 
+        int[] kredse = new int[1000];
+        for (int i = 0; i < kredse.length; i++) {
+            Puslespil puslespil = new Puslespil(n);
+            kredse[i] = puslespil.findKreds();
+        }
+        
+        int[] kredsCounts = new int[n];
         for (int i = 0; i < n; i++) {
-            intArray[i] = i;
+            int count = 0;
+            for (int j = 0; j < kredse.length; j++) {
+                if (kredse[j] == i) {
+                    count++;
+                }
+            }
+            kredsCounts[i] = count;
         }
 
-        List<Integer> intList = Arrays.asList(intArray);
-        Collections.shuffle(intList);
+        System.out.println(Arrays.toString(kredsCounts));
+        System.out.println(spil.H());
 
-        intList.toArray(intArray);
-
-		System.out.println(Arrays.toString(intArray));
-        int kreds = findKreds(intArray);
-        System.out.println(kreds);
+    }
+    public Puslespil(int size) {
+        intArray = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            intArray[i] = i;
+        }
+        intArray = shuffleArray(intArray);
     }
 
-    private static List<Integer> followKreds(Integer[] array, int index) {
+    private Integer[] shuffleArray(Integer[] array) {
+        List<Integer> intList = Arrays.asList(array);
+        Collections.shuffle(intList);
+
+        array = intList.toArray(array);
+        return array;
+    }
+
+    public double H() {
+        double average = 1;
+        for (int i = 2; i < intArray.length; i++) {
+            average = average + (double) 1/i;
+        }
+        return average;
+    }
+
+    private List<Integer> followKreds(Integer[] array, int index) {
         boolean endOfKreds = false;
         List<Integer> seen = new ArrayList<Integer>();
         while (!endOfKreds) {
@@ -44,15 +77,13 @@ public class Puslespil {
         return seen;
     }
 
-    public static int findKreds(Integer[] array) {
+    public int findKreds() {
         List<Integer> found = new ArrayList<Integer>();
         int kreds = 0;
-        for(int i = 0; i < array.length; i++) {
+        for(int i = 0; i < intArray.length; i++) {
             
-            // add number to found
-            // and look at the kreds
-            if (!found.contains(array[i])) {    
-                List<Integer> seen = followKreds(array, i);
+            if (!found.contains(intArray[i])) {    
+                List<Integer> seen = followKreds(intArray, i);
                 found.addAll(seen);
                 kreds++;
             }
