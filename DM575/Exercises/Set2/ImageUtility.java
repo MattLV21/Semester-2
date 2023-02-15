8ad8a0d5f4b7d90d0c834fd645169d80676b7e4e
MattLV21 <Mattias.lange00@gmail.com>
2023-02-15 11:16:17 +0100

    ,

--- DM575/Exercises/Set2/ImageUtility.java
+++ DM575/Exercises/Set2/ImageUtility.java
@@ -1,25 +1,26 @@
-package DM575.Exercises.Set2;
-
-public class ImageUtility {
-    
+public class ImageUtility {    
     public static void main(String[] args) {
         ImageUtility uitility = new ImageUtility();
-        Image myImage = fromFile("myImage.jpg");
-        myImage.display();
-        uitility.encrypt(myImage, "This is an encrypted image");
-        myImage.display();
+        Image myImage = Image.fromFile("myImage.jpg");
+        
+        Image myEncryptedImage = uitility.encrypt(myImage, " ");
+        myEncryptedImage.display();
     }
 
     private Color addColor(Color current, Color second, int extra) {
         Color newColor = new Color((current.red() + second.red() + extra) % 256, (current.green() + second.green() + extra) % 256, (current.blue() + second.blue() + extra) % 256);
         return newColor;
     }
-    public void encrypt(Image image, String key) {
+    private Color subColor(Color current, Color second, int extra) {
+        Color newColor = new Color((current.red() - second.red() - extra) * 256, (current.green() - second.green() - extra) * 256, (current.blue() - second.blue() - extra) * 256);
+        return newColor;
+    }
+    public Image encrypt(Image image, String key) {
         int Index = 0;
         int prevX = 0, prevY = 0;
         Image encryptedImage = new Image(image.width(), image.height());
-        for (int x = 0; x < image.width; x++) {
-            for (int y = 0; y < image.height; y++) {
+        for (int x = 0; x < image.width(); x++) {
+            for (int y = 0; y < image.height(); y++) {
                 Color currentColor = image.pixel(x, y);
                 Color oldColor = image.pixel(prevX, prevY);
                 
@@ -32,7 +33,27 @@ public class ImageUtility {
                 prevY = y;
             }
         }
+        return encryptedImage;
     }
-    
+    public Image decrypt(Image image, String key) {
+        int Index = 0;
+        int prevX = 0, prevY = 0;
+        Image decryptedImage = new Image(image.width(), image.height());
+        for (int x = 0; x < image.width(); x++) {
+            for (int y = 0; y < image.height(); y++) {
+                Color currentColor = image.pixel(x, y);
+                Color oldColor = image.pixel(prevX, prevY);
+                
+                int code = key.codePointAt(Index % key.length());
+                
+                decryptedImage.setPixel(x, y, addColor(currentColor, oldColor, code));
+                
+                Index++;
+                prevX = x; 
+                prevY = y;
+            }
+        }
+        return decryptedImage;
+    }
    
 }
